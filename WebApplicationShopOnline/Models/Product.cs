76d47eab0a1fc30b6fd4cc486a0b30dec7d3f71e
Макаренко <mahadrum@yandex.ru>
2025-02-24 22:22:16 +0300

    Создание полного цикла MVC

---
+++ WebApplicationShopOnline/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApplicationShopOnline.Models
+{
+    public class Product
+    {
+        static int instanceCounter = 0;
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Description { get; set; } 
+        public decimal Cost { get; set; }
+
+        public Product(string name, string description, decimal cost)
+        {
+            Id = instanceCounter;
+            Name = name;
+            Description = description;
+            Cost = cost;
+            instanceCounter++;
+        }
+
+        public override string ToString()
+        {
+            return String.Join("\n", new String[]{Id.ToString(), Name, Cost.ToString(), Description });
+        }
+    }
+}

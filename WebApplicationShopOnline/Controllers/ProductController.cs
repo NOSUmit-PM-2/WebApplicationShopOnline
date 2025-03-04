37cb3ea1c4f05360f9d53aedec752b4e7e5b3ccd
Бдайциев Сармат <bdaitsievsarmat@gmail.com>
2025-03-04 10:38:18 +0300

    добавлены модель и контроллер

---
+++ WebApplicationShopOnline/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationShopOnline.Models;
+
+namespace WebApplicationShopOnline.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product prod = new Product("пирожок", "с ливером", 50);
+            //ViewBag.Product = prod;
+            return View(prod);
+        }
+
+        /*public string Index()
+        {
+            Product prod = new Product("пирожок","с ливером", 50);
+            return prod.ToString();
+        }*/
+    }
+}

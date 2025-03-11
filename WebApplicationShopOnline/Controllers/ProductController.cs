ae4fce1764e727f07348c7e1a5c11ab7b76af485
Макаренко <mahadrum@yandex.ru>
2025-03-11 03:56:01 +0300

    добавлен репозиторий продуктов и страницы его отображения

--- WebApplicationShopOnline/Controllers/ProductController.cs
+++ WebApplicationShopOnline/Controllers/ProductController.cs
@@ -1,28 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationShopOnline.Data;
 using WebApplicationShopOnline.Models;
 
 namespace WebApplicationShopOnline.Controllers
 {
     public class ProductController : Controller
     {
-        //public string Index(string name, decimal cost, string description)
-        //{
-        //    //Product prod = new Product("Пирожок", "Печенный пирожок с мясом", 100);
-        //    Product prod = new Product(name, description, cost);
-        //    return prod.ToString();
-        //}
-
-        //public IActionResult Index(string name, decimal cost, string description)
-        //{
-        //    Product prod = new Product(name, description, cost);
-        //    ViewBag.Product = prod;
-        //    return View();
-        //}
+        private readonly ProductRepository catalog = new ProductRepository();
 
         public IActionResult Index(string name, decimal cost, string description)
         {
-            Product prod = new Product(name, description, cost);
-            return View(prod);
+
+            Product product = catalog.TryGetById(1);
+            if (product != null) 
+                  return View(product);
+            return null;
+        }
+
+        public IActionResult Catalog()
+        {
+            var products = catalog.GetProducts();
+            //return View("CatalogSimple", products);
+            return View(products);
         }
     }
 }
---
+++ WebApplicationShopOnline/Data/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using WebApplicationShopOnline.Models;
+
+namespace WebApplicationShopOnline.Data
+{
+    public class ProductRepository
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product("Чебурек", "с сыром", 120),
+            new Product("Пирожок", "печеный", 50),
+            new Product("Шаурма", "детская", 320)
+        };
+
+        public List<Product> GetProducts()
+        { 
+            return products;
+        }
+
+        public Product TryGetById(int id) 
+        {
+            return products.FirstOrDefault(product => product.Id == id);
+        }
+    
+
+    }
+}

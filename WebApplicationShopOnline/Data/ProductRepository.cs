86660aaff8f2d2f08345affcc0facbddf3aaa247
Макаренко <mahadrum@yandex.ru>
2025-03-18 00:19:24 +0300

    Добавлено хранение картинок локально

--- WebApplicationShopOnline/Data/ProductRepository.cs
+++ WebApplicationShopOnline/Data/ProductRepository.cs
@@ -6,7 +6,7 @@ namespace WebApplicationShopOnline.Data
     {
         private static List<Product> products = new List<Product>()
         {
-            new Product("Чебурек", "с сыром", 120, "https://sinyora.ru/images/img_products/full_Ms_Chebureki.jpg"),
+            new Product("Чебурек", "с сыром", 120, "/images/Chebureki.jpg"),
             new Product("Пирожок", "печеный", 50, "https://cdn.foodpicasso.com/assets/2022/05/30/2d5c997edd5a1c47de33aa05a85dc987---png_1000x_103c0_convert.png"),
             new Product("Шаурма", "детская", 320, "https://bistromania.ru/wp-content/uploads/2022/02/%D1%88%D0%B0%D1%83%D1%80%D0%BC%D0%B0-%D0%B8-%D1%88%D0%B0%D1%83%D1%80%D0%BC%D0%B0-%D0%BC%D0%B8%D0%BD%D0%B8.jpg")
         };

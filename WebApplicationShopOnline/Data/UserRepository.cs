4402045686649037b53dd674dcc44f01e91cc24e
Istiik <istiik@yandex.ru>
2025-04-01 01:17:07 +0300

    создание класса UserRepository

---
+++ WebApplicationShopOnline/Data/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using WebApplicationShopOnline.Models;
+using System.Linq;
+
+namespace WebApplicationShopOnline.Data
+{
+    public class UserRepository
+    {
+        static List<User> users = new List<User>
+        {
+            new User("Аслан", "aslan", "aas12", "+89280000000", "aslan@yandex.ru"),
+            new User("Кира", "kira", "rikk", "+89281000000", "rikitiki2@yandex.ru")
+        };
+
+        public List<User> GetAll()
+        {
+            return users;
+        }
+
+        public User TryGetById(int id)
+        {
+            return users.FirstOrDefault(user => user.IdUser == id);
+        }
+    }
+}

0c70d2412ce77556ad9f2c0ed9d7e6782318fec0
Nistor_Danil <danil.nistor@gmail.com>
2025-04-01 02:09:31 +0300

    Выполнил задание №6. Репозиторий пользователей, поиск по имени.

--- WebApplicationShopOnline/Controllers/UserController.cs
+++ WebApplicationShopOnline/Controllers/UserController.cs
@@ -5,33 +5,69 @@ namespace WebApplicationShopOnline.Controllers
 {
     public class UserController : Controller
     {
-        // Действие для отображения данных пользователя
-        public IActionResult Details()
+        private readonly UserRepository _userRepository;
+        public UserController()
         {
-            // Получаем query-параметры из URL
-            int idUser = int.Parse(Request.Query["IdUser"]);
-            string name = Request.Query["Name"];
-            string login = Request.Query["Login"];
-            string password = Request.Query["Password"];
-            string telephone = Request.Query["Telephone"];
-            string email = Request.Query["Email"];
+            _userRepository = new UserRepository();
+        }
 
-            var user = new User
+        // Действие для отображения данных пользователя
+        //public IActionResult Details()
+        //{
+        //    // Получаем query-параметры из URL
+        //    int idUser = int.Parse(Request.Query["IdUser"]);
+        //    string name = Request.Query["Name"];
+        //    string login = Request.Query["Login"];
+        //    string password = Request.Query["Password"];
+        //    string telephone = Request.Query["Telephone"];
+        //    string email = Request.Query["Email"];
+
+        //    var user = new User
+        //    {
+        //        IdUser = idUser,
+        //        Name = name,
+        //        Login = login,
+        //        Password = password,
+        //        Telephone = telephone,
+        //        Email = email
+        //    };
+
+        //    return View(user);
+        //}
+
+        //public IActionResult Users()
+        //{
+        //    return View();
+        //}
+
+
+        public IActionResult Users(string searchName)
+        {
+            if (!string.IsNullOrEmpty(searchName))
             {
-                IdUser = idUser,
-                Name = name,
-                Login = login,
-                Password = password,
-                Telephone = telephone,
-                Email = email
-            };
+                var user = _userRepository.FindUserByName(searchName);
+                if (user != null)
+                {
+                    return View("Details", user);
+                }
+                else
+                {
+                    ViewBag.Message = "Такой пользователь не найден";
+                    return View(_userRepository.GetAllUsers());
+                }
+            }
+            return View(_userRepository.GetAllUsers());
+        }
 
+        public IActionResult Details(int id)
+        {
+            var user = _userRepository.GetUserById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
             return View(user);
         }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
+        
     }
 }

7b0d4b3ec621f07800a258970399393afa3375f7
Istiik <istiik@yandex.ru>
2025-03-31 20:18:12 +0300

    MVS-User

---
+++ WebApplicationShopOnline/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+using WebApplicationShopOnline.Data;
+using WebApplicationShopOnline.Models;
+
+namespace WebApplicationShopOnline.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index(string name, string login, string password, string phone, string email)
+        {
+            User user = new User(name, login, password, phone, email);
+            return View(user);
+        }
+    }
+}

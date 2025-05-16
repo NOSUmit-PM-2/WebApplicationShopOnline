3d0c49902a07305241761e8f70025ee0e6bb0ffe
Мария <mahadrum@yandex.ru>
2025-05-16 10:03:18 +0300

    Простой класс без свойств

---
+++ WinFormSession/WinFormSession/Exam.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormSession
+{
+    public class Exam
+    {
+        string subject;
+        string date;
+
+        public Exam(string subject)
+        {
+            this.subject = subject;
+        }
+
+        public bool SetDate(string date)
+        {
+            string[] temp = date.Split('.');
+            int day = Convert.ToInt32(temp[0]);
+            int month = Convert.ToInt32(temp[1]);
+            int year = Convert.ToInt32(temp[2]);
+            if (9 < day && day <= 30)
+                if (month == 6)
+                    if (year == 25 || year == 2025)
+                    {
+                        this.date = date;
+                        return true;
+                    }
+            return false;
+        }
+
+        public string GetSubject() 
+        { 
+            return this.subject;
+        }
+
+        public string GetDate() 
+        {
+            return date;
+        }
+
+        public override string ToString()
+        {
+            return this.subject + " " + this.date;
+        }
+    }
+}
---
+++ WinFormSession/WinFormSession/FormSession.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormSession
+{
+    public partial class FormSession : Form
+    {
+        List<Exam> exams = new List<Exam>();
+        void ShowListExams()
+        { 
+            listBoxExams.Items.Clear();
+            foreach (var item in exams)
+            {
+                listBoxExams.Items.Add(item);
+            }
+        }
+        public FormSession()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAddSubject_Click(object sender, EventArgs e)
+        {
+            exams.Add(new Exam("Алгебра"));
+            ShowListExams();    
+        }
+
+        private void buttonSetDate_Click(object sender, EventArgs e)
+        {
+            int currentIndex = listBoxExams.SelectedIndex;
+            if (exams[currentIndex].SetDate("30.06.25"))
+            {
+                ShowListExams();
+            }
+            else
+            {
+                MessageBox.Show("Неверная дата");
+            }
+        }
+    }
+}

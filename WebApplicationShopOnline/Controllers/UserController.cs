bb1e29ec5da3083a804746b385a89006ca71dc6b
Geor <geor107@yandex.ru>
2025-06-06 00:10:09 +0300

    Сделал 6 задачу "репозиторий пользователей:"

---
+++ WebApplicationShopOnline/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationShopOnline.Data;
+
+namespace WebApplicationShopOnline.Controllers
+{
+    public class UserController : Controller
+    {
+        UserRepository userRepository = new UserRepository();
+
+        public IActionResult Index(Guid idUser)
+        {
+            var existingUser = userRepository.TryGetById(idUser);
+            return View(existingUser);
+        }
+
+        public IActionResult UsersAll()
+        {
+            var users = userRepository.GetAll();
+            return View(users);
+        }
+
+        public IActionResult Search(string searchName)
+        {
+            var existingUser = userRepository.GetAll().FirstOrDefault(user => user.Name == searchName);
+            if (existingUser == null)
+                return NotFound("Пользователь не найден!");
+            return RedirectToAction(nameof(Index), new { idUser = existingUser.IdUser });
+        }
+    }
+}
---
+++ WebApplicationShopOnline/Models/UserView.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplicationShopOnline.Models
+{
+    public class UserView
+    {
+        public UserView(string name, string login, string password, string phone, string email)
+        {
+            Name = name;
+            Login = login;
+            IdUser = Guid.NewGuid();
+            Password = password;
+            Phone = phone;
+            Email = email;
+        }
+
+        public Guid IdUser { get; set; }
+        public string Name { get; }
+        public string Login { get; }
+        public string Password { get; }
+        public string Phone { get; }
+        public string Email { get; set; }
+    }
+}

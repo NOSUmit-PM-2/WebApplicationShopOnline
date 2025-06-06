45e592f58ecf1bd51510951842d4df8086d70330
Khu9aev <ArsenKhu9aev@gmail.com>
2025-06-06 08:42:05 +0300

    Задание 5

---
+++ WebApplicationShopOnline/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationShopOnline.Models;
+
+namespace WebApplicationShopOnline.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Profile()
+        {
+            // Получаем query-параметры из URL
+            var user = new User
+            {
+                IdUser = int.TryParse(Request.Query["idUser"], out var id) ? id : 0,
+                Name = Request.Query["name"],
+                Login = Request.Query["login"],
+                Password = Request.Query["password"],
+                Telephone = Request.Query["telephone"],
+                Email = Request.Query["email"]
+            };
+
+            return View("Profile", user);
+        }
+    }
+}
---
+++ WebApplicationShopOnline/Models/User.cs
@@ -0,0 +1,12 @@
+﻿﻿namespace WebApplicationShopOnline.Models
+{
+    public class User
+{
+    public int IdUser { get; set; }
+    public string Name { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public string Telephone { get; set; }
+    public string Email { get; set; }
+}
+}

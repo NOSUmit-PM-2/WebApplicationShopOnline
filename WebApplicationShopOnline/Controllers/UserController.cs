22d48b13f18e521554a5a0edf17b2673206d5aa4
Владимир Кудухов <vladimirkuduhov11@gmail.com>
2025-06-06 14:04:42 +0300

    Задача 5

---
+++ WebApplicationShopOnline/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.DB.Models;
+using System.Numerics;
+using System.Xml.Linq;
+using WebApplicationShopOnline.Data;
+using WebApplicationShopOnline.Models;
+
+namespace WebApplicationShopOnline.Controllers
+{
+    public class UserController : Controller
+    {
+        readonly InterfUsersRepository usersRepository;
+        public UserController(InterfUsersRepository usersRepository)
+        {
+            this.usersRepository = usersRepository;
+        }
+        public IActionResult Index(int id)
+        {
+            User user = usersRepository.TryGetById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+        public IActionResult UserList()
+        {
+            var users = usersRepository.GetAll();
+            return View(users);
+        }
+
+        public IActionResult Search(string searchName)
+        {
+            var user = usersRepository.GetAll().FirstOrDefault(user => user.Name == searchName);
+            if (user == null)
+            {
+                return NotFound("Таких не знаем!");
+            }
+            return RedirectToAction("Index", new { id = user.IdUser });
+        }
+    }
+}
---
+++ WebApplicationShopOnline/Models/User.cs
@@ -0,0 +1,28 @@
+﻿namespace WebApplicationShopOnline.Models
+{
+    public class User
+    {
+        private static int _instanceCounter = 0;
+        public int IdUser { get; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public User(string name, string login, string password, string phone, string email)
+        {
+            IdUser = _instanceCounter++;
+            Name = name;
+            Login = login;
+            Password = password;
+            Phone = phone;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return string.Join("\n", IdUser, Name, Login, Password, Phone, Email);
+        }
+    }
+}

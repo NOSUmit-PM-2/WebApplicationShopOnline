c136aec7c4a17fc0254a528bbbbea76e88632cdf
Hetag <kaytmazov.khetag@gmail.com>
2025-06-06 15:02:21 +0300

    Выполнил 6 задачу. Работа с репозиторием пользователй и меню админ выпадаюзее сделал

--- WebApplicationShopOnline/Controllers/UserController.cs
+++ WebApplicationShopOnline/Controllers/UserController.cs
@@ -1,15 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OnlineShop.DB.Models;
+using WebApplicationShopOnline.Data;
 using WebApplicationShopOnline.Models;
 
 namespace WebApplicationShopOnline.Controllers
 {
     public class UserController : Controller
     {
+        UsersRepository userRepository = new UsersRepository();
+
         public IActionResult Index(string name, string login, string password, string telephone, string email)
         {
             var userView = new UserViewModel(name, login, password, telephone, email);
             return View(userView);
+        }
+
+
+        public IActionResult Details(int idUser)
+        {
+            var existingUser = userRepository.GetById(idUser);
+            return View(existingUser);
+        }
+
+        public IActionResult UsersAll()
+        {
+            var users = userRepository.GetUsers();
+            return View(users);
+        }
+
+        public IActionResult Search(string name)
+        {
+            var existingUser = userRepository.GetUsers().FirstOrDefault(user => user.Name == name);
+            if (existingUser == null)
+                return NotFound("такой пользователь не найден!");
+            return RedirectToAction(nameof(Details), new { idUser = existingUser.IdUser });
         }
     }
 }
---
+++ WebApplicationShopOnline/Data/UsersRepository.cs
@@ -0,0 +1,24 @@
+﻿using WebApplicationShopOnline.Models;
+
+namespace WebApplicationShopOnline.Data
+{
+    public class UsersRepository
+    {
+        static List<UserViewModel> users = new List<UserViewModel>
+        {
+            new UserViewModel("Хетаг", "Heka", "Aa123456", "+1311", "HekaGekae@com.cr"),
+            new UserViewModel("David", "Dava213", "Aa123456", "+13413414", "Davidik@mail.com"),
+        };
+
+        public UserViewModel GetById(int userId)
+        {
+            return users.FirstOrDefault(user => user.IdUser == userId);
+        }
+
+        public List<UserViewModel> GetUsers()
+        {
+            return users;
+        }
+
+    }
+}

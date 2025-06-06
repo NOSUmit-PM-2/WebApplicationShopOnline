82395178ea0eb5c10a6ade81c2c9bbb50dbac5d0
mareta065444 <mareta065444@gmail.com>
2025-06-06 23:08:28 +0300

    задание №5

---
+++ WebApplicationShopOnline/Controllers/UserController1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationShopOnline.Models;
+
+
+namespace WebApplicationShopOnline.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: User/Details
+        public IActionResult Details(int idUser, string name, string login,
+                                   string password, string telephone, string email)
+        {
+            var user = new User1
+            {
+                IdUser = idUser,
+                Name = name,
+                Login = login,
+                Password = password,
+                Telephone = telephone,
+                Email = email
+            };
+            return View(user);
+        }
+    }
+}
---
+++ WebApplicationShopOnline/Models/User1.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplicationShopOnline.Models
+{
+    public class User1
+    {
+        public int IdUser { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+    }
+}

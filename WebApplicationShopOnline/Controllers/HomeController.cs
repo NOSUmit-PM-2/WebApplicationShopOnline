6f60f76c8026652b7f25ab782f16af98c8616f23
tsaga <geor.tsaga@yandex.ru>
2025-06-06 11:40:43 +0300

    5 задача

--- WebApplicationShopOnline/Controllers/HomeController.cs
+++ WebApplicationShopOnline/Controllers/HomeController.cs
@@ -9,26 +9,6 @@ namespace WebApplicationShopOnline.Controllers
             return View();
         }
 
-        public string Hello()
-        {
-            var currentTime = DateTime.Now.TimeOfDay;
-
-            if (currentTime >= new TimeSpan(0, 0, 0) && currentTime < new TimeSpan(6, 0, 0))
-            {
-                return "Доброй ночи";
-            }
-            else if (currentTime >= new TimeSpan(6, 0, 0) && currentTime < new TimeSpan(12, 0, 0))
-            {
-                return "Доброе утро";
-            }
-            else if (currentTime >= new TimeSpan(12, 0, 0) && currentTime < new TimeSpan(18, 0, 0))
-            {
-                return "Добрый день";
-            }
-            else
-            {
-                return "Добрый вечер";
-            }
-        }
+        
     }
 }
